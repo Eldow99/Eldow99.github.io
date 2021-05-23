7440c04d4e2d1a6a060ed16962f8d3bdba4e45a2
Eldow99 <61770737+Eldow99@users.noreply.github.com>
2021-05-23 21:36:31 +0600

    week2

---
+++ week3/js/main.js
@@ -0,0 +1,36 @@
+// console.log("ELdiyar")
+
+// let x = 0;
+// while(x<10){
+//     console.log(x)
+//     x++;
+// }
+// let i=2;
+// for(;i<=12;i++){
+ 
+//     if (i == 10) continue;
+//        console.log(i)
+// }
+// console.log(`${i}+"ITOG"`)
+
+//1
+// let num = 8;
+// while(num){
+//     console.log(num)
+//     num--;
+// }
+
+//2 
+// let num = 0;
+// while(num<3){
+//     console.log(`Number : ${num}`)
+//     num++;
+// }
+
+//3
+first:for(let num = 0;num<2; num++ ){
+    for(let size = 0;size < 3; size++){
+        console.log(size)
+        if (size=1)break first
+    }
+}
